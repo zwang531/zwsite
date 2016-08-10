f92a0b3f10962efdc721dbce44ad4afbe1b7422e
Zongwei Wang <zow003@ucsd.edu>
2016-08-10 09:11:39 -0700

    new deploy.php

--- deploy.php
+++ deploy.php
@@ -1,40 +1,46 @@
+ed and is available at [yoursite].com/deploy.php
+
 <?php
+    /**
+     * GIT DEPLOYMENT SCRIPT
+     *
+     * Used for automatically deploying websites via GitHub
+     *
+     */
 
-	$commands = array(
-		'echo $PWD',
-		'whoami',
-		'git pull',
-		'git status',
-		'git submodule sync',
-		'git submodule update',
-		'git submodule status',
-	);
+    // array of commands
+    $commands = array(
+        'echo $PWD',
+        'whoami',
+        'git pull',
+        'git status',
+        'git submodule sync',
+        'git submodule update',
+        'git submodule status',
+    );
 
-	$output = '';
-	foreach($commands AS $command){
-		// Run it
-		$tmp = shell_exec($command);
-		// Output
-		$output .= "<span style=\"color: #6BE234;\">\$</span> <span style=\"color: #729FCF;\">{$command}\n</span>";
-		$output .= htmlentities(trim($tmp)) . "\n";
-	}
-
+    // exec commands
+    $output = '';
+    foreach($commands AS $command){
+        $tmp = shell_exec($command);
+        
+        $output .= "<span style=\"color: #6BE234;\">\$</span><span style=\"color: #729FCF;\">{$command}\n</span><br />";
+        $output .= htmlentities(trim($tmp)) . "\n<br /><br />";
+    }
 ?>
+
 <!DOCTYPE HTML>
 <html lang="en-US">
 <head>
-	<meta charset="UTF-8">
-	<title>GIT DEPLOYMENT SCRIPT</title>
+    <meta charset="UTF-8">
+    <title>GIT DEPLOYMENT SCRIPT</title>
 </head>
 <body style="background-color: #000000; color: #FFFFFF; font-weight: bold; padding: 0 10px;">
-<pre>
- .  ____  .    ____________________________
- |/      \|   |                            |
-[| <span style="color: #FF0000;">&hearts;    &hearts;</span> |]  | Git Deployment Script v0.1 |
- |___==___|  /              &copy; zwsite 2016 |
-              |____________________________|
-
-<?php echo $output; ?>
-</pre>
+<div style="width:700px">
+    <div style="float:left;width:350px;">
+    <p style="color:white;">Git Deployment Script</p>
+    <?php echo $output; ?>
+    </div>
+</div>
 </body>
 </html>
