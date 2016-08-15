7ab6c5dd3c0029a8c214c3c124a8066adbb7ff3c
Zongwei Wang <zow003@ucsd.edu>
2016-08-15 15:17:38 -0700

    fixed hello world cgi

--- cgi/hello_world.pl
+++ cgi/hello_world.pl
@@ -4,6 +4,7 @@ my ($sec, $min, $hour, $mday, $mon, $year, $wday, $yday, $isdst) = localtime(tim
 my @months = qw(Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec);
 my @days = qw(Sunday Monday Tuesday Wednesday Thursday Friday Saturday);
 
+my $yr = $year + 1990;
 my $range = 2;
 my $val = int(rand($range));
 my $color = "yellow";
@@ -12,12 +13,12 @@ if($val == 0){
     $color = "red";
 }elsif($val == 1){
     $color = "white";
-}else{
+}elsif($val == 2){
     $color = "blue";
 }
 
 print "Content-type: text/html\n\n";
 print "<html><head><title>Hello World</title>";
 print "</head><body bgcolor='$color'>\n";
-print "<h1>Hello Web World from Language Perl on $days[$wday], $months[$mon] $mday, $year</h1>\n";
+print "<h1>($val) Hello Web World from Language Perl on $days[$wday], $months[$mon] $mday, $yr</h1>\n";
 print "</body>\n</html>";
