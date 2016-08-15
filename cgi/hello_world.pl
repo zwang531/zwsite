06427e724507441662f91bddcb558979470c2b04
Zongwei Wang <zow003@ucsd.edu>
2016-08-15 13:11:48 -0700

    hello world cgi

---
+++ cgi/hello_world.pl
@@ -0,0 +1,7 @@
+#!/usr/bin/perl -wT
+
+print "Content-type: text/html\n\n";
+print "<html><head><title>Hello World</title>";
+print "</head><body bgcolor='yellow'>\n";
+print "<h1>Hello from CGI</h1>\n";
+print "</body>\n</html>";
