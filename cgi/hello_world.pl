359365eddc70bba051b2037e15b99b7dc8376174
Zongwei Wang <zow003@ucsd.edu>
2016-08-15 14:33:46 -0700

    hello world cgi

--- cgi/hello_world.pl
+++ cgi/hello_world.pl
@@ -1,7 +1,23 @@
 #!/usr/bin/perl -wT
+use Switch;
+
+my ($sec, $min, $hour, $mday, $mon, $year, $wday, $yday, $isdst) = localtime(time);
+my @months = qw(Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec);
+my @days = qw(Sunday Monday Tuesday Wednesday Thursday Friday Saturday);
+
+my $range = 2;
+my $val = int(rand($range));
+my $color = "yellow";
+
+switch($val){
+    case 0 {$color = "red"}
+    case 1 {$color = "white"}
+    case 2 {$color = "blue"}
+    else {}
+}
 
 print "Content-type: text/html\n\n";
 print "<html><head><title>Hello World</title>";
-print "</head><body bgcolor='yellow'>\n";
-print "<h1>Hello from CGI</h1>\n";
+print "</head><body bgcolor='$color'>\n";
+print "<h1>Hello Web World from Language Perl on $days[$wday], $months[$mon] $mday, $year</h1>\n";
 print "</body>\n</html>";
