f0c1eaf3e5b4a68505312d09cece17f6569ef46e
Zongwei Wang <zow003@ucsd.edu>
2016-08-16 10:28:26 -0700

    add scripts

---
+++ cgi/env.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl -wT
+
+print "Content-type: text/html\n\n";
+print <<END;
+<!DOCTYPE html>
+<html>
+<head><title>Environment Variables Echo</title>
+<style>
+        header {
+            background-color: black;
+            padding: 1em;
+            text-align: center;
+            color: white;
+        }
+</style></head>
+<body><header><h1>Environment Variables</h1></header><br>
+END
+
+foreach $var (sort keys %ENV){
+    printf "%s = \"%s\"<br>", $var, $ENV{$var};
+}
+print <<END;
+<hr><p style="font-style: italic;"><a href="/index.html">Back to home</a></p>
+</body>
+</html>
+END
---
+++ cgi/hello.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl -wT
+
+my ($sec, $min, $hour, $mday, $mon, $year, $wday, $yday, $isdst) = localtime(time);
+my @months = qw(Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec);
+my @days = qw(Sunday Monday Tuesday Wednesday Thursday Friday Saturday);
+
+my $yr = $year + 1900;
+my $range = 3;
+my $val = int(rand($range));
+my $color = "yellow";
+
+if($val == 0){
+    $color = "red";
+}elsif($val == 1){
+    $color = "white";
+}elsif($val == 2){
+    $color = "blue";
+}
+
+print "Content-type: text/html\n\n";
+print "<html><head><title>Hello World</title>";
+print "</head><body bgcolor='$color'>\n";
+print "<h1>Hello Web World from Language Perl on $days[$wday], $months[$mon] $mday, $yr</h1>\n";
+print "</body>\n</html>";
