1cf73e09048a2f6002416eea6244963bf3cebe39
Zongwei Wang <zow003@ucsd.edu>
2016-08-16 22:55:58 -0700

    php

--- env.php
+++ env.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
 <html>
-   <head>
-    <meta charset="utf-8">
-    <title>Environment Variables Echo</title>
-   </head>
+    <head>
+	<meta charset="utf-8">
+	<title>environment variables echo</title>
+    </head>
 
-   <body>
-    <script language="php">
-        print("<pre>\n");
-        print_r($_SERVER);
-        print("</pre>\n");
-    </script>
-   </body>
+    <body>
+	<script language="php">
+	    print("<pre>\n");
+	    print_r($_SERVER);
+	    print("</pre>\n");
+	</script>
+    </body>
 </html>
--- hello.php
+++ hello.php
@@ -2,7 +2,7 @@
 <html>
     <head>
 	<meta charset="utf-8">
-	<title>hello.php</title>
+	<title>hello world</title>
     </head>
 
     <script language="php">
