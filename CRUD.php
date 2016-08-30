9b7e8197a517fc3ea007cb502c6ad5986993db11
Zongwei Wang <zow003@ucsd.edu>
2016-08-30 13:18:59 -0700

    list view

--- CRUD.php
+++ CRUD.php
@@ -40,7 +40,7 @@
     
       while($row = mysqli_fetch_assoc($result)) {
 	    print "<tr>";
-	    print "<td><img src=\"". $row['poster'] . "\" alt=\"Poster\"></td>" ;
+	    print "<td><img src=\"". $row['poster'] . "\" alt=\"Poster\" width=60, height=90></td>" ;
 	    print "<td>". $row['movie_title'] . "</td>" ;
 	    print "<td>". $row['studio_name'] . "</td>" ;
 	    print "<td>". $row['year'] . "</td>" ;
