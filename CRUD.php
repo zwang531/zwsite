e917a8f29dbf40875726cb5d14a631614c81447f
Zongwei Wang <zow003@ucsd.edu>
2016-08-31 10:05:00 -0700

    paging

--- CRUD.php
+++ CRUD.php
@@ -123,6 +123,8 @@
 	$max_page = 1;
 ?>
 <div class='btn-group'>
+    
+<div class='btn-group' name='nav'>
     <!-- pagination markup -->
     <nav aria-label="Page navigation">
       <ul class="pagination">
@@ -190,13 +192,17 @@
 	    <li role="presentation"><a role="menuitem" tabindex="-1" href="CRUD.php?entry=all">All</a></li>
 	</ul>
     </div>
-</div> <!-- end of button group -->
+</div> <!-- end of nav button group -->
     
 <br>
 
+<div>
 <form action="/edit" method="POST" style="float:right;">
 	<input type="submit" name="action" value="Add" class="btn btn-lg btn-primary">
 </form>	
+</div>
+
+</div> <!-- end of nav button group -->
 
 <br><br>
 <hr>
