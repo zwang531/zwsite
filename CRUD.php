83bbea47abab595d015db5b866cfa61e0c8d9a50
Zongwei Wang <zow003@ucsd.edu>
2016-08-30 20:03:26 -0700

    sql alerts

--- CRUD.php
+++ CRUD.php
@@ -16,7 +16,7 @@
     
 <div class="alert alert-danger alert-dismissible" role="alert">
   <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-  <strong>Oh snap!</strong> Change a few things up and try submitting again.
+    <p id="top_alert"></p>
 </div>
 
 <?php
