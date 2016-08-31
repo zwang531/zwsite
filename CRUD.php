e73ce596de3eafe77355ddd07da977da269047db
Zongwei Wang <zow003@ucsd.edu>
2016-08-31 10:11:41 -0700

    paging

--- CRUD.php
+++ CRUD.php
@@ -15,7 +15,7 @@
 	
 <h1>Box Office</h1>
 
-<form action="/edit" method="POST">
+<br><form action="/edit" method="POST">
 	<input type="submit" name="action" value="Add" class="btn btn-primary">
 </form><br>  
         
