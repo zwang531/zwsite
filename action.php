95f690ead35451be5392e4bcb043e5326646f1ed
Zongwei Wang <zow003@ucsd.edu>
2016-08-31 00:53:05 -0700

    remove modal

--- action.php
+++ action.php
@@ -17,6 +17,7 @@
     $upload = false;
     $target_dir = 'img/';
     $filename = '';
+    $err = '';
 
     if ($action == 'Add' || $action == 'Update'){
         $filename = basename($_FILES["poster"]["name"]);
@@ -42,7 +43,6 @@
 	   
 	   // SHOULD HAVE VALIDATION HERE!?
         
-        $err = '';
         if($movie_title != strip_tags($movie_title) || $studio_name != strip_tags($studio_name)){
             if($err != '') $err .= "<br>";
             $err .= "Text Fields contain HTMP or PHP tags!";
@@ -79,7 +79,6 @@
         
        $id = $_REQUEST['id'];
         
-        $err = '';
         if($movie_title != strip_tags($movie_title) || $studio_name != strip_tags($studio_name)){
             if($err != '') $err .= "<br>";
             $err .= "Text Fields contain HTMP or PHP tags!";
