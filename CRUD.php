2279955133f2dc0bbdc820be28bcb9713cd7c0cb
Zongwei Wang <zow003@ucsd.edu>
2016-08-31 21:21:07 -0700

    error alerts

--- CRUD.php
+++ CRUD.php
@@ -196,7 +196,7 @@
       <div class="modal-content">
         <div class="modal-header">
           <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-          <h4 class="modal-title">Warning!</h4>
+          <h4 class="modal-title">Confirm Delete?</h4>
         </div>
         <div class="modal-body">
           <p>Do you really want to delete it permanently?</p>
--- action.php
+++ action.php
@@ -16,9 +16,9 @@
         $filename = basename($_FILES["poster"]["name"]);
         if(strlen($filename) > 65){
             $err = "Invalid inputs:<br>Upload File Name is resctircted to maximum 65 charactors!";
-            die($err);
+            //die($err);
         }
-        if($filename != ''){
+        if($filename != '' && $err != ''){
             $target_file = $target_dir . $filename;
             if (file_exists($target_file))
                 $poster = '' . $target_file;
@@ -64,9 +64,9 @@
         }
         if($err != '') {
             $err = "Invalid inputs:<br>" . $err;
-            die($err);
+            //die($err);
         }
-        
+        if($err != ''){
 		if ($upload == true){
             $sql = "INSERT INTO movies (movie_title,studio_name,year,dollar_value,poster) VALUES ('$movie_title' , '$studio_name' , '$year' , '$dollar_value', '$target_file')";
         }
@@ -75,7 +75,7 @@
         }
         
 	   $result = mysqli_query($conn, $sql);
-		
+       }
 		
 	} else if ($action == "Update") {
         
@@ -112,7 +112,7 @@
             $err = "Invalid inputs:<br>" . $err;
             die($err);
         }
-                
+        if($err != ''){        
         if ($upload == true){
             $sql = "UPDATE movies SET movie_title='" .$movie_title."' ,studio_name='".$studio_name."' ,year='".$year."' ,dollar_value='".$dollar_value."' ,poster='".$target_file."' WHERE id='".$id."'";
         }
@@ -121,7 +121,7 @@
         }
 	
        $result = mysqli_query($conn, $sql);
-		
+        }
 	}  else if ($action == "Delete") {
         
         $id = $_POST['id'];
@@ -140,7 +140,48 @@
 
 		
 	}
-
-	header('Location: CRUD.php');
+    
+    if($err!='')
+	   header('Location: CRUD.php');
 	
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Alerts!</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="container">
+
+    <?php 
+    
+        $msg = strtok($err, '\n');
+    
+        do{
+            $tmp = ''.$msg;
+            $msg = strtok('\n');
+            
+            if($msg !== false){
+                print "<div class='alert alert-danger'>";
+                print "<a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a><strong>Invalid input:</strong> ".$tmp."</div>";
+            }
+            else{
+                print "<div class='alert alert-danger'>";
+                print "<a href='/CRUD' class='close' data-dismiss='alert' aria-label='close'>&times;</a><strong>Invalid input:</strong> ".$tmp."</div>";
+                break;
+            }
+            
+        }while ($msg !== false) 
+    ?>
+  
+</div>
+
+</body>
+</html>
