0cc380926dd59ee4386fe21c4b72ba03698fc66a
Zongwei Wang <zow003@ucsd.edu>
2016-08-31 17:43:18 -0700

    delete modal

--- CRUD.php
+++ CRUD.php
@@ -227,7 +227,7 @@
     
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-<script>
+<script type="text/javascript">
     
     var SBC = {};
     
@@ -259,6 +259,12 @@
         });		
         
 	}; /* doDelete */
+    
+    $(document).ready(function () {
+		
+		SBC.loadData();
+					
+    });
     
 </script>
 
