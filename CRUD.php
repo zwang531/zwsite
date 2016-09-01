d0776561f7eab432f3cd55d0c391051a38dbc47a
Zongwei Wang <zow003@ucsd.edu>
2016-08-31 17:31:26 -0700

    delete modal

--- CRUD.php
+++ CRUD.php
@@ -5,8 +5,6 @@
 <title>CRUD</title>
 
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     
 </head>
 <body>
@@ -227,7 +225,10 @@
     </div>
   </div>
     
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <script>
+    
     var SBC = {};
     
     SBC.confirmDelete = function confirmDelete(id) {
